4246b51556fa2f13fe20632512e6d981b275a8bc
LiviaAndreea <32998064+LiviaAndreea@users.noreply.github.com>
2018-06-22 12:43:09 +0300

    Update ClkDivider.v

--- ClkDivider.v
+++ ClkDivider.v
@@ -1,15 +1,26 @@
+
+
 module ClkDivider (
 
-    input EN,
-	 input clock_div,
-    output clk,
-    input rst
+    input [3:0] MODE,
+    input clk,
+    input D,
+    input rst,
+    output reg clk_div,
+	
  );
-    
-
-
- endmodule
-
+ 
+ wire clock_25;
+ wire clock_50;
+    always@(posedge clock) begin
+	 
+		if (D == 1) 
+		clk_div <= clk_div;
+		else 
+		clk_div <= ~clk_div;
+		
+end
+endmodule
  
 
 
